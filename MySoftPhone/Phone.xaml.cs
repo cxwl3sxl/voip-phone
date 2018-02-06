7de18abb2b182269c59238cc25ec3d6c32e7e2e3
cxwl3sxl <xinglin.sc@outlook.com>
2018-02-06 12:32:44 +0800

    电话以及电话代理完成，准备实现主子进程模式

--- MySoftPhone/Phone.xaml.cs
+++ MySoftPhone/Phone.xaml.cs
@@ -1,19 +1,9 @@
-﻿using Ozeki.VoIP;
-using Ozeki.VoIP.SDK;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Ozeki.Media.MediaHandlers;
 
 namespace MySoftPhone
 {
@@ -23,16 +13,7 @@ namespace MySoftPhone
     public partial class Phone : UserControl, IDisposable
     {
         public event Action<Phone> OnRemove;
-
-        ISoftPhone softPhone;
-        IPhoneLine phoneLine;
-        IPhoneCall call;
-        private Microphone microphone = Microphone.GetDefaultDevice();
-        private Speaker speaker = Speaker.GetDefaultDevice();
-        MediaConnector connector = new MediaConnector();
-        PhoneCallAudioSender mediaSender = new PhoneCallAudioSender();
-        PhoneCallAudioReceiver mediaReceiver = new PhoneCallAudioReceiver();
-        int currentDtmfSignal;
+        private PhoneProxy _phoneProxy;
 
         public Phone()
         {
@@ -46,19 +27,7 @@ namespace MySoftPhone
 
         private void buttonHangUp_Click(object sender, RoutedEventArgs e)
         {
-            if (call != null)
-            {
-                if (call.IsIncoming && call.CallState.IsRinging())
-                {
-                    call.Reject();
-                }
-                else
-                {
-                    call.HangUp();
-                }
-
-                call = null;
-            }
+            _phoneProxy?.HangUp();
             LabelNumber.Content = string.Empty;
         }
 
@@ -68,7 +37,7 @@ namespace MySoftPhone
             if (btn == null)
                 return;
 
-            if (call != null)
+            if (_phoneProxy.HaveCall)
                 return;
 
             LabelNumber.Content += btn.Content.ToString().Trim();
@@ -76,10 +45,10 @@ namespace MySoftPhone
 
         private void buttonKeyPad_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            if (call == null)
+            if (_phoneProxy.HaveCall)
                 return;
 
-            if (!call.CallState.IsInCall())
+            if (!_phoneProxy.IsInCall)
                 return;
 
             var btn = sender as Button;
@@ -87,8 +56,7 @@ namespace MySoftPhone
             if (dtmfSignal == -1)
                 return;
 
-            currentDtmfSignal = dtmfSignal;
-            call.StartDTMFSignal((DtmfNamedEvents)dtmfSignal);
+            _phoneProxy.SendSignal(dtmfSignal);
         }
 
         private int GetDtmfSignalFromButtonTag(Button button)
@@ -108,108 +76,12 @@ namespace MySoftPhone
 
         private void buttonPickUp_Click(object sender, RoutedEventArgs e)
         {
-            // accept incoming call
-            if (call != null && call.IsIncoming)
-            {
-                call.Answer();
-                return;
-            }
-
-            // dial
-            if (call != null)
-                return;
-
-            if (string.IsNullOrEmpty(LabelNumber.Content?.ToString()))
-                return;
-
-            if (phoneLine.RegState != RegState.RegistrationSucceeded)
-            {
-                MessageBox.Show("Phone line must be registered!");
-                return;
-            }
-
-            call = softPhone.CreateCallObject(phoneLine, LabelNumber.Content.ToString());
-            SubscribeToCallEvents(call);
-            call.Start();
-        }
-
-        private void SubscribeToCallEvents(IPhoneCall call)
-        {
-            if (call == null)
-                return;
-
-            call.CallStateChanged += (call_CallStateChanged);
-            call.DtmfReceived += (call_DtmfReceived);
+            _phoneProxy.PickUp(LabelNumber.Content?.ToString());
         }
 
         private void InvokeGUIThread(Action action)
         {
             Dispatcher.Invoke(action, null);
-        }
-
-        private void call_CallStateChanged(object sender, CallStateChangedArgs e)
-        {
-            InvokeGUIThread(() =>
-            {
-                LabelMessage.Content = e.State.ToString();
-                switch (e.State)
-                {
-                    case CallState.Ringing:
-                        LabelMessage.Background = new SolidColorBrush(Colors.Red);
-                        break;
-                    case CallState.InCall:
-                        LabelMessage.Background = new SolidColorBrush(Colors.Green);
-                        break;
-                    default:
-                        LabelMessage.Background = new SolidColorBrush(Colors.White);
-                        break;
-                }
-            });
-
-            if (e.State == CallState.Answered)
-            {
-                if (microphone != null)
-                    microphone.Start();
-
-                if (speaker != null)
-                    speaker.Start();
-
-                mediaSender.AttachToCall(call);
-                mediaReceiver.AttachToCall(call);
-                return;
-            }
-
-            if (e.State.IsCallEnded())
-            {
-                if (microphone != null)
-                    microphone.Stop();
-
-                if (speaker != null)
-                    speaker.Stop();
-
-                mediaSender.Detach();
-                mediaReceiver.Detach();
-
-                UnsubscribeFromCallEvents(sender as IPhoneCall);
-                call = null;
-
-                InvokeGUIThread(() => { LabelNumber.Content = string.Empty; });
-            }
-        }
-
-        private void UnsubscribeFromCallEvents(IPhoneCall call)
-        {
-            if (call == null)
-                return;
-
-            call.CallStateChanged -= (call_CallStateChanged);
-            call.DtmfReceived -= (call_DtmfReceived);
-        }
-
-        private void call_DtmfReceived(object sender, VoIPEventArgs<DtmfInfo> e)
-        {
-            DtmfSignal signal = e.Item.Signal;
-            InvokeGUIThread(() => LabelMessage.Content = String.Format("DTMF signal received: {0} ", signal.Signal));
         }
 
         private void Setting_OnClick(object sender, RoutedEventArgs e)
@@ -226,36 +98,6 @@ namespace MySoftPhone
 
         private void Phone_OnLoaded(object sender, RoutedEventArgs e)
         {
-            //try
-            //{
-            //    softPhone = SoftPhoneFactory.CreateSoftPhone(15000, 15500);
-            //    softPhone.IncomingCall += softPhone_IncomingCall;
-            //    phoneLine = softPhone.CreatePhoneLine(new SIPAccount(true, "1001", "1001", "1001", "12345", "192.168.0.33", 5060));
-            //    phoneLine.RegistrationStateChanged += phoneLine_PhoneLineStateChanged;
-
-            //    softPhone.RegisterPhoneLine(phoneLine);
-
-            //    ConnectMedia();
-            //}
-            //catch (Exception ex)
-            //{
-            //    var sb = new StringBuilder();
-            //    sb.AppendLine("Error while initializing softphone.");
-            //    sb.AppendLine();
-            //    sb.AppendLine("Exception:");
-            //    sb.AppendLine(ex.Message);
-            //    sb.AppendLine();
-            //    if (ex.InnerException != null)
-            //    {
-            //        sb.AppendLine("Inner Exception:");
-            //        sb.AppendLine(ex.InnerException.Message);
-            //        sb.AppendLine();
-            //    }
-            //    sb.AppendLine("StackTrace:");
-            //    sb.AppendLine(ex.StackTrace);
-
-            //    MessageBox.Show(String.Format("{0}", sb), "Ozeki WPF Softphone Sample", MessageBoxButton.OK, MessageBoxImage.Error);
-            //}
             InitPhone();
         }
 
@@ -285,14 +127,12 @@ namespace MySoftPhone
                 try
                 {
                     Dispose();
-                    softPhone = SoftPhoneFactory.CreateSoftPhone(15000, 15500);
-                    softPhone.IncomingCall += softPhone_IncomingCall;
-                    phoneLine = softPhone.CreatePhoneLine(new SIPAccount(true, Setting.Number, Setting.Number, Setting.Number, Setting.Password, Setting.ServerIp, Setting.Port));
-                    phoneLine.RegistrationStateChanged += phoneLine_PhoneLineStateChanged;
-
-                    softPhone.RegisterPhoneLine(phoneLine);
-
-                    ConnectMedia();
+                    LabelClientInfo.Text = Setting.Number;
+                    LabelClientServer.Text = Setting.ServerIp;
+                    _phoneProxy = new PhoneProxy(Setting.Number, Setting.Password, Setting.ServerIp, Setting.Port);
+                    _phoneProxy.RaiseMessage += _phoneProxy_RaiseMessage;
+                    _phoneProxy.StateChanged += _phoneProxy_StateChanged;
+                    _phoneProxy.StateMessageChanged += _phoneProxy_StateMessageChanged;
                 }
                 catch (Exception ex)
                 {
@@ -316,70 +156,61 @@ namespace MySoftPhone
             }
         }
 
-        public PhoneSetting Setting { get; set; }
-
-
-
-        private void ConnectMedia()
+        private void _phoneProxy_StateMessageChanged(string obj)
         {
-            if (speaker != null)
-                connector.Connect(mediaReceiver, speaker);
-
-            if (microphone != null)
-                connector.Connect(microphone, mediaSender);
+            InvokeGUIThread(() =>
+            {
+                LabelStatus.Content = obj;
+            });
         }
+
+        private void _phoneProxy_StateChanged(PhoneState obj)
+        {
+            InvokeGUIThread(() =>
+            {
+                switch (obj)
+                {
+                    case PhoneState.Ringing:
+                        LabelMessage.Background = new SolidColorBrush(Colors.Red);
+                        break;
+                    case PhoneState.InCall:
+                        LabelMessage.Background = new SolidColorBrush(Colors.Green);
+                        break;
+                    case PhoneState.CallEnded:
+                    case PhoneState.Other:
+                        LabelMessage.Background = new SolidColorBrush(Colors.White);
+                        LabelNumber.Content = string.Empty;
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException(nameof(obj), obj, null);
+                }
+            });
+        }
+
+        private void _phoneProxy_RaiseMessage(string obj)
+        {
+            InvokeGUIThread(() =>
+            {
+                LabelMessage.Content = obj;
+            });
+        }
+
+        public PhoneSetting Setting { get; set; }
 
         private void buttonKeyPad_MouseUp(object sender, MouseButtonEventArgs e)
         {
-            if (call == null)
+            if (!_phoneProxy.HaveCall)
                 return;
 
-            if (!call.CallState.IsInCall())
+            if (!_phoneProxy.IsInCall)
                 return;
 
-            call.StopDTMFSignal((DtmfNamedEvents)currentDtmfSignal);
-        }
-
-        private void phoneLine_PhoneLineStateChanged(object sender, RegistrationStateChangedArgs e)
-        {
-            InvokeGUIThread(() =>
-            {
-                var account = ((IPhoneLine)sender).SIPAccount;
-                LabelClientInfo.Text = account.RegisterName;
-                LabelClientServer.Text = account.DomainServerHost;
-                if (e.State == RegState.RegistrationSucceeded)
-                {
-                    LabelStatus.Content = "Online";
-                }
-                else
-                    LabelStatus.Content = e.State.ToString();
-            });
-        }
-
-        private void softPhone_IncomingCall(object sender, VoIPEventArgs<IPhoneCall> e)
-        {
-            var incomingCall = e.Item as IPhoneCall;
-            if (call != null)
-            {
-                incomingCall.Reject();
-                return;
-            }
-
-            call = incomingCall;
-            SubscribeToCallEvents(call);
-
-            InvokeGUIThread(() =>
-            {
-                LabelMessage.Content = String.Format("Incoming call from {0}", e.Item.DialInfo.CallerDisplay);
-                LabelMessage.Background = new SolidColorBrush(Colors.Red);
-            });
+            _phoneProxy.StopSignal();
         }
 
         public void Dispose()
         {
-            call?.HangUp();
-            phoneLine?.Dispose();
-            softPhone?.Close();
+            _phoneProxy?.Stop();
         }
     }
 }
---
+++ MySoftPhone/PhoneProxy.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MySoftPhone
+{
+    public enum PhoneState
+    {
+        Ringing,
+        InCall,
+        CallEnded,
+        Other
+    }
+
+    public class PhoneProxy
+    {
+        public void Start()
+        {
+        }
+
+        public void Stop()
+        {
+        }
+
+        private readonly RealPhone _realPhone;
+        public PhoneProxy(string number, string password, string ip, int port)
+        {
+            _realPhone = new RealPhone(number, password, ip, port);
+            _realPhone.RaiseMessage += _realPhone_RaiseMessage;
+            _realPhone.StateChanged += _realPhone_StateChanged;
+            _realPhone.StateMessageChanged += _realPhone_StateMessageChanged;
+        }
+
+        private void _realPhone_StateMessageChanged(string obj)
+        {
+            StateMessageChanged?.Invoke(obj);
+        }
+
+        private void _realPhone_StateChanged(PhoneState obj)
+        {
+            StateChanged?.Invoke(obj);
+        }
+
+        private void _realPhone_RaiseMessage(string obj)
+        {
+            RaiseMessage?.Invoke(obj);
+        }
+
+        /// <summary>
+        /// 发送按键信号
+        /// </summary>
+        /// <param name="signal"></param>
+        public void SendSignal(int signal)
+        {
+            _realPhone.SendSignal(signal);
+        }
+
+        /// <summary>
+        /// 停止发送信号
+        /// </summary>
+        public void StopSignal()
+        {
+            _realPhone.StopSignal();
+        }
+
+        /// <summary>
+        /// 拨号或者接听
+        /// </summary>
+        /// <param name="number"></param>
+        public void PickUp(string number)
+        {
+            _realPhone.PickUp(number);
+        }
+
+        /// <summary>
+        /// 挂断
+        /// </summary>
+        public void HangUp()
+        {
+            _realPhone.HangUp();
+        }
+
+        /// <summary>
+        /// 电话状态发生变化
+        /// </summary>
+        public event Action<PhoneState> StateChanged;
+
+        /// <summary>
+        /// 推送消息
+        /// </summary>
+        public event Action<string> RaiseMessage;
+
+        /// <summary>
+        /// 状态发生变化的消息
+        /// </summary>
+        public event Action<string> StateMessageChanged;
+
+        public bool HaveCall => _realPhone.HaveCall;
+        public bool IsInCall => _realPhone.IsInCall;
+    }
+}
