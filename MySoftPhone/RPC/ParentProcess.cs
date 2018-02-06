787a2b42f9cde1bfd4ff976f5bcca8916c806617
cxwl3sxl <xinglin.sc@outlook.com>
2018-02-06 09:49:58 +0800

    集成匿名管道完成

---
+++ MySoftPhone/RPC/ParentProcess.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MySoftPhone.RPC
+{
+    public class ParentProcess : IDisposable
+    {
+        private readonly int _parentProcessId;
+        private bool _stop;
+        private readonly ConcurrentQueue<string> _messageConcurrentQueue = new ConcurrentQueue<string>();
+        private readonly AutoResetEvent _autoResetEvent = new AutoResetEvent(false);
+        private readonly PipeStream _pipeClient;
+
+        public event Action ParentProcessExit;
+
+        public ParentProcess(string[] args)
+        {
+            Console.WriteLine(string.Join(" ", args));
+            if (args.Length > 1 && args[0].StartsWith("$") && args[1].StartsWith("$"))
+            {
+                _pipeClient = new AnonymousPipeClientStream(PipeDirection.Out, args[0].Substring(1));
+                if (_pipeClient == null) throw new Exception("参数无效，无法和主进程通信！");
+                _pipeClient.ReadMode = PipeTransmissionMode.Byte;
+                _parentProcessId = Convert.ToInt32(args[1].Substring(1));
+                InputArgs = args.Skip(2).ToArray();
+                Task.Factory.StartNew(DoSend);
+            }
+            else
+            {
+                InputArgs = args;
+            }
+        }
+
+        public string[] InputArgs { get; }
+
+        public void Dispose()
+        {
+            _stop = true;
+        }
+
+        public void SendMessage(string message)
+        {
+            _messageConcurrentQueue.Enqueue(message);
+            _autoResetEvent.Set();
+        }
+
+        void DoSend()
+        {
+            using (StreamWriter sw = new StreamWriter(_pipeClient))
+            {
+                while (!_stop)
+                {
+                    try
+                    {
+                        Process.GetProcessById(_parentProcessId);
+                    }
+                    catch
+                    {
+                        ParentProcessExit?.Invoke();
+                    }
+                    _autoResetEvent.Reset();
+                    while (_messageConcurrentQueue.TryDequeue(out var msg))
+                    {
+                        Console.WriteLine("正在写入消息" + msg);
+                        sw.WriteLine(msg);
+                        sw.Flush();
+                        _pipeClient.WaitForPipeDrain();
+                    }
+                    _autoResetEvent.WaitOne(1000);
+                }
+            }
+        }
+    }
+}
---
+++ MySoftPhone/RPC/SubProcess.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Pipes;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MySoftPhone.RPC
+{
+    public class SubProcess
+    {
+        private readonly ProcessStartInfo _processStartInfo;
+        private AnonymousPipeServerStream _anonymousPipeServerStream;
+        private StreamReader _pipeReader;
+        private bool _isSubProcessExit;
+        private Process _subProcess;
+        private readonly string _inputArgs;
+
+        public SubProcess(ProcessStartInfo processStartInfo)
+        {
+            _inputArgs = processStartInfo.Arguments;
+            _processStartInfo = processStartInfo;
+            _processStartInfo.UseShellExecute = false;
+        }
+
+        public void Start()
+        {
+            _anonymousPipeServerStream?.Dispose();
+            _anonymousPipeServerStream =
+                new AnonymousPipeServerStream(PipeDirection.In, HandleInheritability.Inheritable)
+                {
+                    ReadMode = PipeTransmissionMode.Byte
+                };
+            _pipeReader?.Dispose();
+            _pipeReader = new StreamReader(_anonymousPipeServerStream);
+            var newArgs =
+                $"${_anonymousPipeServerStream.GetClientHandleAsString()} ${Process.GetCurrentProcess().Id} {_inputArgs}";
+            _processStartInfo.Arguments = newArgs;
+            _subProcess = new Process
+            {
+                StartInfo = _processStartInfo,
+                EnableRaisingEvents = true
+            };
+            _subProcess.Exited += SubProcess_Exited;
+            _subProcess.Start();
+            _isSubProcessExit = false;
+            _anonymousPipeServerStream.DisposeLocalCopyOfClientHandle();
+            Task.Factory.StartNew(ReadMessage);
+        }
+
+        private void SubProcess_Exited(object sender, EventArgs e)
+        {
+            if (!_isSubProcessExit)
+                SubProcessExited?.Invoke(this);
+        }
+
+        void ReadMessage()
+        {
+            while (!_isSubProcessExit)
+            {
+                var msg = _pipeReader?.ReadLine();
+                if (!string.IsNullOrWhiteSpace(msg))
+                    MessageReceived?.Invoke(this, msg);
+                Thread.Sleep(500);
+            }
+        }
+
+        public void Stop()
+        {
+            _isSubProcessExit = true;
+            _anonymousPipeServerStream?.Dispose();
+            _pipeReader?.Dispose();
+            _subProcess.Kill();
+        }
+
+        public int? ProcessId => _subProcess?.Id;
+
+        public event Action<SubProcess> SubProcessExited;
+        public event Action<SubProcess, string> MessageReceived;
+    }
+}
