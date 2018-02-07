4d76ffa7657b8e41d920eb07f8875cfe855dfa00
cxwl3sxl <xinglin.sc@outlook.com>
2018-02-07 10:20:42 +0800

    清理文件

---
+++ MySoftPhone/AboutWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MySoftPhone
+{
+    /// <summary>
+    /// AboutWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class AboutWindow : Window
+    {
+        public AboutWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void UIElement_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Process.Start("https://github.com/cxwl3sxl/voip-phone");
+        }
+    }
+}
